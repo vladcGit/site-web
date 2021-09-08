bd6fdd180d7b539b5227797a3ad2bb1b57d2038c
vladcGit <81162853+vladcGit@users.noreply.github.com>
2021-09-08 20:24:17 +0300

    cleanup dependenta inutila

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -20,17 +20,6 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
     ],
   },
   optimization: {
